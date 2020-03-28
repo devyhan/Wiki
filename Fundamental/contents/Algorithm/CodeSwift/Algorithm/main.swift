7f309c2cb77bfa552622839f529052a89cc846bb
devyhan93 <devyhan93@gmail.com>
2020-03-28 16:39:22 +0900

    200328_1001번 문제

--- Fundamental/contents/Algorithm/CodeSwift/Algorithm/main.swift
+++ Fundamental/contents/Algorithm/CodeSwift/Algorithm/main.swift
@@ -42,3 +42,14 @@ let a = Int(lineArr[0])!
 let b = Int(lineArr[1])!
 print(a+b)
 */
+/*=======================================
+                Q_1001
+=======================================*/
+/*
+import Foundation
+let line = readLine()!
+let lineArr = line.components(separatedBy: " ")
+let a = Int(lineArr[0])!
+let b = Int(lineArr[1])!
+print(a-b)
+*/
