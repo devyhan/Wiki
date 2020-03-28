7309b861863684e2f6fab8091ab3fd0d08cdad1b
devyhan93 <devyhan93@gmail.com>
2020-03-28 16:38:27 +0900

    200328_1000번 문제

--- Fundamental/contents/Algorithm/CodeSwift/Algorithm/main.swift
+++ Fundamental/contents/Algorithm/CodeSwift/Algorithm/main.swift
@@ -31,3 +31,14 @@ print("( 0 )\"\"\"\\")
 print("|\"^\"`    |")
 print("||_/=\\\\__|")
 */
+/*=======================================
+                Q_1000
+=======================================*/
+/*
+import Foundation
+let line = readLine()!
+let lineArr = line.components(separatedBy: " ")
+let a = Int(lineArr[0])!
+let b = Int(lineArr[1])!
+print(a+b)
+*/
